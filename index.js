29b5d71835d5958ae3d076736b24001d2b8e4554
Artem <unbywyd@gmail.com>
2022-04-28 16:26:03 +0300

    +

---
+++ index.js
@@ -0,0 +1,234 @@
+const path = require('path');
+let colorFinder = require('./colorFInder');
+const parser = require('postcss-selector-parser');
+const postcss = require('postcss');
+module.exports = {
+    plugin: (opts = {}) => {
+        let options = Object.assign({
+            extract: {},
+            saveProps: false,
+            loaderContext: null,
+            root: null
+        }, opts);
+        options.selector = Object.assign({
+            before: true,
+            as: 'className',
+            value: 'theme',
+            replace: ['html', ':root']
+        }, options.selector);
+
+        let { before, as, value, replace } = options.selector;
+        if (!value || String(value).trim() == '') {
+            throw new Error('selector.value cannot be empty');
+        }
+        let { loaderContext, extract, root: rootPath, saveProps } = options;
+        let rootDeclarations = new Map();
+        let atDeclaration = new Map();
+        const transform = selectors => {
+            const node = parser[as]({ value });
+
+
+            selectors.each(selector => {
+                if (parser[as] === undefined) {
+                    throw new Error(`${as} method not supported. Use one of the following methods: (attribute, className, id, tag)`)
+                }
+                let combo = parser.combinator({ value: ' ' });
+
+
+                /*
+                *   We pass through the selector and check if it contains replaceable parts,
+                *   if so, replace with the required value for the first time
+                *   and any next time, just delete
+                */
+                let replaced = false;
+                selector.each(n => {
+                
+                    if (replace.includes(String(n).toLocaleLowerCase())) {
+                        if (replaced) {                          
+                            n.remove();
+                        } else {
+                            n.replaceWith(node);
+                            replaced = true;
+                        }
+                    }
+                });
+
+                if (!replaced) {
+                    if (!before) {
+                        selector.insertAfter(selector.first, node);
+                    } else {
+                        selector.prepend(combo);
+                        selector.insertBefore(combo, node);
+                    }
+                }
+            });
+        };
+
+        function prepareSelector(selector) {
+            let result = parser().processSync(selector, { lossless: false });
+            let out = parser().processSync(parser(transform).processSync(result), { lossless: false });          
+            return out;
+        }
+        function prepareDecl(decl) {
+            let declParent = decl.parent;
+            let { selector, parent } = decl.parent;
+            let isAtRule = parent.type == 'atrule';
+
+            if (isAtRule) {
+                let name = parent.name;
+                if (name == 'media') {
+                    let slc = prepareSelector(selector);
+
+                    if (!atDeclaration.has(parent.params)) {
+                        atDeclaration.set(parent.params, []);
+                    }
+                    let prev = atDeclaration.get(parent.params);
+                    let prevParent = prev.find(e => e.selector == slc);
+                    if (prevParent) {
+                        prevParent.declarations.push(decl.clone())
+                    } else {
+                        atDeclaration.get(parent.params).push({
+                            selector: slc,
+                            declarations: [decl.clone()]
+                        });
+                    }
+                    if (!saveProps) {
+                        decl.remove();
+
+                        let isEmpty = declParent.nodes.filter(n => n.type != 'comment').length === 0;
+
+                        if (isEmpty) {
+                            declParent.remove(); // Remove empty parent
+                        }
+                        let isEmptyAtRule = parent.nodes.filter(n => n.type != 'comment').length === 0;
+
+                        if (isEmptyAtRule) {
+                            parent.remove(); // Remove empty atRule
+                        }
+                    }
+                }
+                // skip keyframes
+            } else {
+                // Root rules          
+                let slc = prepareSelector(selector);
+                if (!rootDeclarations.has(slc)) {
+                    rootDeclarations.set(slc, []);
+                }
+                rootDeclarations.get(slc).push(decl.clone());
+                if (!saveProps) {
+                    decl.remove();
+                    let isEmpty = declParent.nodes.filter(n => n.type != 'comment').length === 0
+                    if (isEmpty) {
+                        declParent.remove();
+                    }
+                }
+            }
+        }
+
+        return {
+            postcssPlugin: "postcss-theme-colors-extractor",
+            Declaration(decl) {
+                if (colorFinder(decl.value)) {
+                    prepareDecl(decl);
+                }
+            },
+            OnceExit(root, args) {
+                if (extract) {
+                    let content = '';
+
+                    rootDeclarations.forEach((declarations, selector) => {
+                        content += `${selector} {${declarations.map(e => `${e.prop}: ${e.value};`).join('')}}`
+                    });
+
+                    atDeclaration.forEach((rules, params) => {
+                        let result = '';
+                        rules.forEach(rule => {
+                            let { declarations, selector } = rule;
+                            result += `${selector} {${declarations.map(e => `${e.prop}: ${e.value};`).join('')}}`
+                        });
+
+                        content += `@media ${params} {${result}}`;
+                    });
+                    if ('object' == typeof extract) {
+
+                        let relativePath = path.relative(rootPath || loaderContext.rootContext, opts.loaderContext.resourcePath);
+
+                        let exFileName = path.basename(relativePath, path.extname(relativePath));
+                        let prefix = path.normalize(path.dirname(relativePath)).replace(/[\/\\]+/g, '-');
+
+                        let readyName = prefix + '-' + exFileName;
+
+                        let fileNameTemplate = extract.fileName ? extract.fileName : '[name]-theme-[suffix].css';
+
+                        let fileName = fileNameTemplate.replace(/\[name\]/gi, readyName);
+                        fileName = fileName.replace(/\[suffix\]/gi, value);
+                        args.result.messages.push({
+                            type: "asset",
+                            file: fileName.toLowerCase(),
+                            content
+                        });
+                    } else if ('function' == typeof extract) {
+                        extract({
+                            content,
+                            loaderContext
+                        });
+                    }
+                } else {
+                    rootDeclarations.forEach((declarations, selector) => {
+                        let node = postcss.rule();
+                        node.selector = selector;
+                        node.nodes = declarations;
+                        root.append(node);
+                    });
+                    atDeclaration.forEach((rules, params) => {
+                        let atRule = postcss.atRule();
+                        atRule.name = 'media';
+                        atRule.params = params;
+
+                        let nodes = [];
+                        rules.forEach(rule => {
+                            let { declarations, selector } = rule;
+                            let node = postcss.rule();
+                            node.selector = selector;
+                            node.nodes = declarations;
+                            nodes.push(node);
+                        });
+
+                        atRule.nodes = nodes;
+
+                        root.append(atRule);
+                    });
+                }
+            }
+        };
+    },
+    ungicSassThemeIntegrator({ themeName = 'default', themesPath=path.join(__dirname, 'app', 'themes'), themeOptions = {}, includeAs = '*' }) {
+        return function (content, loaderContext) {
+            const { resourcePath, rootContext } = loaderContext;
+
+            const pathToTheme = path.join(path.relative(path.relative(rootContext, path.dirname(resourcePath)), themesPath), themeName + '.scss').replace(/[\/\\]+/g, '/');
+
+            let sassOptionsList = [];
+            for (let key in themeOptions) {
+                sassOptionsList.push(`${key}: ${themeOptions[key]}`);
+            }
+            let sassMap = `(${sassOptionsList.join(',')})`;
+
+            let output = `
+              @use "sass:meta";
+              @use "sass:map";
+              @use "${pathToTheme}" as ungic-theme-config;
+              $ungic-theme-config: meta.module-variables(ungic-theme-config);
+
+              $ungic-theme-config: map.merge($ungic-theme-config, ${sassMap});   
+              
+              @use "ungic-sass-theme" as ${includeAs} with (                      
+                $theme: $ungic-theme-config
+              );  
+              ${content} 
+              @include render-vars();`
+
+            return output;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,231 @@
+# Theme colors extractor
+
+This is a postcss plugin that extracts colors from css styles into new css rules with changed selector, example:
+
+```css
+/* Before */
+.btn {
+    display: block;
+    padding: 1em;
+    font-size: 2em;
+    color: #000;
+    border: 3px solid #000;
+}
+
+/* After */
+.btn {
+    display: block;
+    padding: 1em;
+    font-size: 2em;
+}
+.default-theme .btn {
+    color: #000;
+    border: 3px solid #000;
+}
+```
+
+## Features of this plugin
+
+* Extract all css colors
+* Selectors wrapper (you can add prefixes to selectors to create multiple themes) 
+* Extract to external files via webpack assets
+* Extract to external files via custom function
+* Support and integration for [ungic-sass-theme] module
+
+
+## Get Started 
+
+Install `npm install theme-colors-extractor`
+
+
+Use in your webpack project:
+
+```js
+// Webpack configuration
+const { plugin } = require('./postcss-plugin.js');
+
+let isProductionMode = env.production;
+
+module: {
+    rules: [
+        {
+            test: /\.scss$/,
+            use: [
+                "style-loader",
+                {
+                    loader: "css-loader",
+                    options: {
+                    url: false,
+                    import: false,
+                    },
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        postcssOptions: {
+                            plugins: [
+                                require("postcss-rtl")(),
+                                require("autoprefixer")()
+                                plugin(
+                                    selector: {
+                                        before: true, // Add before each selector
+                                        as: 'className', // class
+                                        value: 'default-theme', // with default-theme name
+                                        replace: ['html', ':root', 'body', '[dir]'], // Replace root elements to theme name
+                                    }
+                                );
+                            ]
+                        }
+                    }
+                },
+                {
+                    loader: "sass-loader",
+                    options: {}
+                }
+            ],
+        },
+    ],
+}
+```
+
+## Ungic SASS theme integration + Extract to external files
+
+```js
+// Webpack configuration
+const { plugin, ungicSassThemeIntegrator } = require('./postcss-plugin.js');
+
+let isProductionMode = env.production;
+
+module: {
+    rules: [
+        {
+            test: /\.scss$/,
+            use: [
+                isProductionMode ? MiniCssExtractPlugin.loader : "style-loader",
+                {
+                    loader: "css-loader",
+                    options: {
+                    url: false,
+                    import: false,
+                    },
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        postcssOptions: (loaderContext) => {
+                            let plugins = [
+                                require("postcss-rtl")(),
+                                require("autoprefixer")()
+                            ]
+
+                            if (isProductionMode) {
+                                plugins.push(plugin({
+                                    loaderContext,
+                                    root: path.join(__dirname, 'app'),
+                                    extract: { // Or function
+                                        fileName: '[name]-theme'+ (env.inverse? '-inv' : '') + '-[suffix].css', // Save as
+                                    },
+                                    selector: {
+                                        before: true,
+                                        as: 'className',
+                                        value: env.inverse ? env.theme + '-inv' : env.theme, // Theme name
+                                        replace: ['html', ':root', 'body', '[dir]'], // Replace root elements to theme name
+                                    }
+                                }))
+                            }
+
+                            return {
+                                plugins
+                            }
+                        }
+                    }
+                },
+                {
+                    loader: "sass-loader",
+                    options: {
+                        implementation: require("sass"),
+                        additionalData: ungicSassThemeIntegrator({ // Not required
+                            themeName: env.theme,
+                            themeOptions: {
+                                'inverse-mode': !!env.inverse
+                            }
+                        }),
+                        sassOptions: {
+                            outputStyle: "expanded",
+                            sourceComments: true
+                        },
+                    },
+                }
+            ],
+        },
+    ],
+}
+```
+
+
+
+## Options
+
+
+### extract
+
+This option is used to extract colors, `not required`
+
+- **Type:** `function` or `object`
+- **as object:**
+	- **fileName** - output file name, By default:`[name]-theme-[suffix].css`
+    
+    **fileName** - can contain two replaceable keys:
+        - **[name]** - source scss file name
+        - **[suffix]** - value from selector.value parameter of configuration
+    ```js
+    plugin({
+        extract: { // Or function
+            fileName: '[name]-theme'+ (env.inverse? '-inv' : '') + '-[suffix].css', // Save as
+        },
+        ...
+    })
+    ```
+
+- **as function:**
+    ```js
+        plugin({
+            extract: function({content, loaderContext}) {
+                // here you can export the resulting colors
+            }
+            ...
+        })
+    ```
+
+### saveProps
+
+Remove colors in original scss files? By default, colors are removed from source files, `not required`
+
+- **Type:** `boolean`
+- **Default:** `false`
+
+
+### loaderContext
+
+Webpack `loaderContext`, `required` for `extract` option!
+
+### root
+
+Root path of application, if not specified, `loaderContext.rootContext` will be used 
+- **Type:** `string` - full path (for example: `path.join(__dirname, 'app')`)
+
+### selector (required)
+
+Customizing the Selector for Exported Style Rules
+
+- **Type:** `object`
+    - **before** `boolean`, - selector handling method
+        - `true` **default**  - Add prefix to each selector (.theme-name .app) 
+        - `false` - Merge prefix with root of selector (.app.theme-name)
+    - **as** `string` - type of prefix
+        - `attribute` 
+        - `className` **default**
+        - `id` 
+        - `tag` 
+    - **value** `string` - name of prefix, **default:** *theme*
+    - **replace** `array` - what should be replaced with selector.value, **default:** `['html', ':root']`
